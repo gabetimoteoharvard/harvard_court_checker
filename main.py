1345b8aca062bfb5809123dab74812494d99c7d5
GABRIEL <gabrieltimoteo@college.harvard.edu>
2025-11-23 00:02:08 -0500

    environment for chrome

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import time
 import json
 import datetime
 from emailer import send_email
+import os
 
 
 def setup_driver():
@@ -20,7 +21,7 @@ def setup_driver():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
 
-    options.binary_location = "/usr/bin/chromium-browser"
+    options.binary_location = os.environ.get("CHROME_BIN")
 
     driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()),
                              options= options)
